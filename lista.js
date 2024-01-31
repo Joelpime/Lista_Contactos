c47325387ccfc21f3b25da6b4e6a05827c1c107b
Jorge Pinzon <jorge.pinzon18@gmail.com>
2024-01-30 20:25:38 -0500

    nueva lista

---
+++ lista.js
@@ -0,0 +1,33 @@
+const listaContactos = [
+    {
+        identificacion: 1,
+        nombre: "Jorge Eliecer",
+        apellidos: "Pinzon Mendivelso",
+        telefono: 123465,
+        ciudad: "Bogota",
+    },
+    {
+        identificacion: 2,
+        nombre: "Emiliano Andrés",
+        apellidos: "Pinzon",
+        telefono: 56789,
+        ciudad: "Bogota",
+    }
+];
+
+function crearContacto(identificacion, nombre, apellidos, telefono, ciudad) {
+    const nuevoContacto = {
+        identificacion: identificacion,
+        nombre: nombre,
+        apellidos: apellidos,
+        telefono: telefono,
+        ciudad: ciudad,
+    };
+
+    listaContactos.push(nuevoContacto);
+
+}
+
+crearContacto(98734, "Emma Isabella", "Pinzón", 3434502, "Bogotá");
+
+console.log(listaContactos);
